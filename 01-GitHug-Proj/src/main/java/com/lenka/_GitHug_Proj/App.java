6283fadaefacd4e94e47c1afe5c41fb62b436935
Subrajit07 <subrajitlenka1234@gmail.com>
2025-02-08 11:33:02 +0530

    Initial commit TL

---
+++ 01-GitHug-Proj/src/main/java/com/lenka/_GitHug_Proj/App.java
@@ -0,0 +1,13 @@
+package com.lenka._GitHug_Proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
