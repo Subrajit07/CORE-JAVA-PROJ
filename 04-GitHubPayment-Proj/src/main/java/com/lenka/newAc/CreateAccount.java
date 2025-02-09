b617711a0b049d4c06b7050534f9f84c4c833897
Subrajit07 <subrajitlenka1234@gmail.com>
2025-02-09 12:08:11 +0530

    TL- Apply Card

--- 04-GitHubPayment-Proj/src/main/java/com/lenka/newAc/CreateAccount.java
+++ 04-GitHubPayment-Proj/src/main/java/com/lenka/newAc/CreateAccount.java
@@ -3,4 +3,6 @@ package com.lenka.newAc;
 public interface CreateAccount {
 
 	public String create(String name,long adhar,long phno);
+	
+	public String applyCard(long accno,long adhar);
 }
--- 04-GitHubPayment-Proj/src/main/java/com/lenka/newAc/CreateAccountImpl.java
+++ 04-GitHubPayment-Proj/src/main/java/com/lenka/newAc/CreateAccountImpl.java
@@ -6,10 +6,15 @@ public class CreateAccountImpl implements CreateAccount {
 
 	@Override
 	public String create(String name, long adhar, long phno) {
-		if(name!=null) {
-			return "Account is created ACNo :: "+new Random().nextLong(1700000000);
+		if (name != null) {
+			return "Account is created ACNo :: " + new Random().nextLong(1700000000);
 		}
 		return "Invalid credentials";
 	}
 
+	@Override
+	public String applyCard(long accno, long adhar) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
