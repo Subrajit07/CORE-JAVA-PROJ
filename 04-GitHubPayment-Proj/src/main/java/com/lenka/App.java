b6433987843cf37838cab5c6add58f0573444e1d
Subrajit07 <subrajitlenka1234@gmail.com>
2025-02-09 11:44:41 +0530

    TL- initial commit

---
+++ 04-GitHubPayment-Proj/src/main/java/com/lenka/App.java
@@ -0,0 +1,15 @@
+package com.lenka;
+
+import com.lenka.upi.UPIPayment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      
+    }
+}
---
+++ 04-GitHubPayment-Proj/src/main/java/com/lenka/upi/UPIPayment.java
@@ -0,0 +1,6 @@
+package com.lenka.upi;
+
+public interface UPIPayment {
+
+	public String payment(String accno,double amount);
+}
